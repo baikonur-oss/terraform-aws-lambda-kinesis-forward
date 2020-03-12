092f4cd799c942daeed4045df2790730b26b6590
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2020-03-12 18:25:17 +0900

    black format

--- lambda/main.py
+++ lambda/main.py
@@ -11,43 +11,43 @@ from aws_xray_sdk.core import xray_recorder
 # set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-logger.info('Loading function')
+logger.info("Loading function")
 
 # patch boto3 with X-Ray
-libraries = ('boto3', 'botocore')
+libraries = ("boto3", "botocore")
 patch(libraries)
 
 # global client instances
-s3_client = boto3.client('s3')
-kinesis_client = boto3.client('kinesis')
+s3_client = boto3.client("s3")
+kinesis_client = boto3.client("kinesis")
 
 # configure with env vars
-FAILED_LOG_S3_PREFIX: str = os.environ['FAILED_LOG_S3_PREFIX']
-FAILED_LOG_S3_BUCKET: str = os.environ['FAILED_LOG_S3_BUCKET']
+FAILED_LOG_S3_PREFIX: str = os.environ["FAILED_LOG_S3_PREFIX"]
+FAILED_LOG_S3_BUCKET: str = os.environ["FAILED_LOG_S3_BUCKET"]
 
-LOG_ID_FIELD: str = os.environ['LOG_ID_FIELD']
-LOG_TYPE_FIELD: str = os.environ['LOG_TYPE_FIELD']
-LOG_TIMESTAMP_FIELD: str = os.environ['LOG_TIMESTAMP_FIELD']
-LOG_TYPE_UNKNOWN_PREFIX: str = os.environ['LOG_TYPE_UNKNOWN_PREFIX']
+LOG_ID_FIELD: str = os.environ["LOG_ID_FIELD"]
+LOG_TYPE_FIELD: str = os.environ["LOG_TYPE_FIELD"]
+LOG_TIMESTAMP_FIELD: str = os.environ["LOG_TIMESTAMP_FIELD"]
+LOG_TYPE_UNKNOWN_PREFIX: str = os.environ["LOG_TYPE_UNKNOWN_PREFIX"]
 
-LOG_TYPE_FIELD_WHITELIST_TMP: list = str(os.environ['LOG_TYPE_WHITELIST']).split(',')
+LOG_TYPE_FIELD_WHITELIST_TMP: list = str(os.environ["LOG_TYPE_WHITELIST"]).split(",")
 if len(LOG_TYPE_FIELD_WHITELIST_TMP) == 0:
     LOG_TYPE_FIELD_WHITELIST = set()
 else:
     LOG_TYPE_FIELD_WHITELIST = set(LOG_TYPE_FIELD_WHITELIST_TMP)
 
-TARGET_STREAM_NAME: str = os.environ['TARGET_STREAM_NAME']
-KINESIS_MAX_RETRIES: int = int(os.environ['KINESIS_MAX_RETRIES'])
+TARGET_STREAM_NAME: str = os.environ["TARGET_STREAM_NAME"]
+KINESIS_MAX_RETRIES: int = int(os.environ["KINESIS_MAX_RETRIES"])
 
 
 def handler(event, context):
-    raw_records = event['Records']
+    raw_records = event["Records"]
     logger.debug(raw_records)
 
     log_dict = dict()
     failed_dict = dict()
 
-    xray_recorder.begin_subsegment('parse')
+    xray_recorder.begin_subsegment("parse")
     for payload in kinesis.parse_records(raw_records):
         try:
             payload_parsed = json.loads(payload)
@@ -68,14 +68,18 @@ def handler(event, context):
         )
     xray_recorder.end_subsegment()
 
-    xray_recorder.begin_subsegment('kinesis PutRecords')
+    xray_recorder.begin_subsegment("kinesis PutRecords")
     for key in log_dict:
-        logger.info(f"Processing log type {key}: {len(log_dict[key]['records'])} records")
-        records_json = [json.dumps(x) for x in log_dict[key]['records']]
-        kinesis.put_records_batch(kinesis_client, TARGET_STREAM_NAME, records_json, KINESIS_MAX_RETRIES)
+        logger.info(
+            f"Processing log type {key}: {len(log_dict[key]['records'])} records"
+        )
+        records_json = [json.dumps(x) for x in log_dict[key]["records"]]
+        kinesis.put_records_batch(
+            kinesis_client, TARGET_STREAM_NAME, records_json, KINESIS_MAX_RETRIES
+        )
     xray_recorder.end_subsegment()
 
-    xray_recorder.begin_subsegment('s3 upload')
+    xray_recorder.begin_subsegment("s3 upload")
     baikonur_logging.save_json_logs_to_s3(s3_client, failed_dict, reason="Failed logs")
     xray_recorder.end_subsegment()
 
